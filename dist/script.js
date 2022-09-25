d664c94261e683d954c0fdb7239fe816deab764c
Bianca <biancadeoliveiralima26@gmail.com>
2022-09-25 11:13:32 -0300

    Projeto concluído

--- dist/script.js
+++ dist/script.js
@@ -31,3 +31,76 @@ function trocar(){
     document.querySelector('button').classList.add('preto');
 }
 }
+
+
+var nome = "Bianca";
+var idade = 19;
+var salvo = true;
+var cidade;
+
+//Comentário de uma linha
+
+/*Cometário
+     de
+Mútiplaslinhas*/ 
+
+
+//Funções (1-2)
+
+function alterar(){
+    document.getElementById("titulo").innerHTML="Trocou o titulo";
+    document.getElementById("campo").value="Trocou de campo";
+}
+alterar();
+
+//Funções (2-2)
+function alterar(titulo){
+    document.getElementById("titulos").innerHTML=titulo;
+}
+alterar("Título de exemplo");
+
+function somar(x, y){
+    let total=x+y;
+    document.getElementById("campos").value= total;
+}
+
+function somar(x, y){
+    let total=x+y;
+    return total;
+}
+
+var resultado = somar(22,19);
+
+console.log(resultado);
+
+//Manipulando Atributos
+
+function trocarImagem(filename, animalname){
+    document.querySelector(".imagem").setAttribute('src', '../assets/images/'+filename);
+    document.querySelector(".imagem").setAttribute('data-animal', animalname);
+}
+function pegarAnimal(){
+    let animal= document.querySelector('.imagem').getAttribute('data-animal');
+    alert("O animal é: "+animal);
+}
+
+//Distâncias e Scroll Suave
+
+function subirTela(){
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    });
+}
+
+//Resposta do Exercício Scroll
+
+function decidirBotaoScroll(){
+    if(window.scrollY === 0){
+        document.querySelector('.scrollbutton').style.display = 'none';
+    } else {
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+
+window.addEventListener('scroll', decidirBotaoScroll);
